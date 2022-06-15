c2c4d06f696210d5f51d08f8925c15bc0d567453
Caleb Smith <calebsmithwoolrich@gmail.com>
2022-06-15 17:10:47 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use checkssl::CheckSSL;
 
 fn main() {
-    println!("SSL: {:?}", CheckSSL::from_domain("rust-lang.org"));
-   
+    println!("SSL: {:?}", CheckSSL::from_domain("rust-lang.org".to_string()));
+    println!("SSL: {:?}", CheckSSL::from_domain("rusaffdsst-lang.org".to_string()));
+    println!("SSL: {:?}", CheckSSL::from_domain("pixelcoda.com".to_string()));
+    println!("SSL: {:?}", CheckSSL::from_domain("ckitl.com".to_string()));
+    println!("SSL: {:?}", CheckSSL::from_domain("test.ckitl.com".to_string()));
 }
